e64c63cce5037b62e510524f7e797587c6194155
Hobbo <moutaw.i@gmail.com>
2011-09-24 18:56:41 +0200

    lolilol ca marche :D:D

--- lambdacalcul.ml
+++ lambdacalcul.ml
@@ -2,12 +2,9 @@
 (*	Made by		db0			*)
 (*	Contact		db0company@gmail.com	*)
 (*	Website		http://db0.fr		*)
-(*						*)
 
-(*	λx.x					*)
-(*	=> Abs("x", Var x)			*)
-(*	(λx.x)2					*)
-(*	=> App(Abs("x", Var x), Const 2)	*)
+(*	(\x.x)2					*)
+(*	App (Abs ("x", Var "x"), Const 2)	*)
 
 type e =
   | Var		of string
@@ -22,30 +19,38 @@ type res =
   | Closure	of (string * e * env)
   | Error	of string
 
+(*						*)
+(* val getInEnv : string -> env -> e		*)
+(*						*)
 let rec getInEnv x = function
   | []			-> raise (Failure "Undefined identifier")
   | (str, exp)::rest	-> if str = x then exp else getInEnv x rest
 
+
+(*						*)
+(* val setInEnv : env -> string -> e -> env	*)
+(*						*)
 let setInEnv env str e = (str, e)::env
 
-(* let isVar = function *)
-(*   | Var	v	-> true *)
-(*   | _		-> false *)
-
-(* let isClosure = function *)
-(*   | Const c		-> true *)
-(*   | Abs	(s, e)		-> true *)
-(*   | _			-> false *)
-
+(*						*)
+(* val dec : res -> e				*)
+(*						*)
 let dec = function
   | Closure (str, e, env)	-> e
   | Rconst r			-> Const r
-  | _				-> raise (Failure "tata foobar toto test bite")
+  | _				-> raise (Failure "Error on closure application")
 
+
+(*						*)
+(* val apply : res -> res -> env -> res		*)
+(*						*)
 let rec apply res1 res2 env = match res1 with
   | Closure (str, e, env)	-> eval_lambda (setInEnv env str (dec res2)) e
   | _				-> Error ("Expression is not a Closure, U MAD BRO, OH AND BTW... U JUST LOST IT *troll face*")
 
+(*						*)
+(* val eval_lambda : env -> e -> res		*)
+(*						*)
 and eval_lambda env = function
   | Const n		-> Rconst n
   | Var x		-> eval_lambda env (getInEnv x env)
@@ -58,9 +63,8 @@ let main () =
   let e = App (Abs ("x", Var "x"), Const 2) in
       let resultat = eval_lambda [] e in
 	match resultat with
-	  | Rconst n	-> print_int n
-	  | Closure c	-> print_endline "closure"
-	  | Error err	-> print_endline err
+	  | Rconst n			-> print_int n
+	  | Closure (str, e, env)	-> begin print_string "Closure "; print_endline str end
+	  | Error err			-> print_endline err
 
 let _ = main ()
-
