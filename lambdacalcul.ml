b689d8355b36ec5b95ea46607e7eddc81214e0c1
Hobbo <moutaw.i@gmail.com>
2011-09-24 17:19:44 +0200

    app avance o/ The game

--- lambdacalcul.ml
+++ lambdacalcul.ml
@@ -28,25 +28,29 @@ let rec getInEnv x = function
 
 let setInEnv env str e = (str, e)::env
 
+let isVar = function
+  | Var	v	-> true
+  | _		-> false
+
 let isClosure = function
-  | Const		-> true
-  | Abs			-> true
+  | Const c		-> true
+  | Abs	(s, e)		-> true
   | _			-> false
+
+let apply e1 e2 = match e1 with
+  | Closure (str, e, env)	-> aux (setInEnv str e) e2
+  | _				-> Error ("Expression is not a Closure, U MAD BRO, OH AND BTW... U JUST LOST IT *troll face*")
 
 let eval_lambda e =
   let rec aux env = function
     | Const n		-> Rconst n
     | Var x		-> aux env ( getInEnv x env)
     | Abs (str, exp)	-> Closure (str, exp, env)
-    | App (e1, e2)	->
-	  if isClosure e1
-	  then aux (setInEnv env e2 e1) e2
-	  else Error "Expression is not a Closure"
+    | App (e1, e2)	-> apply (aux env e1) (aux env e2)
 in aux [] e
 
 let main () =
-  let x = "x" in
-  let e = App (Abs ("x", Var x), Const 2) in
+  let e = App (Abs ("x", "x"), Const 2) in
     eval_lambda e
 
 let _ = main ()
