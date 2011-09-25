ba65b23de7d2d9576abef6c9ad3329a1497c9313
db0 <zero.fansub@gmail.com>
2011-09-25 14:44:14 +0200

    some tests in da main

--- main.ml
+++ main.ml
@@ -1,37 +1,52 @@
 (*									     *)
-(*	Filename	lambdacalcul.ml					     *)
+(*	Filename	main.ml						     *)
 (*	Project		Lambda-calcul_eval_AST				     *)
 (*	Authors		Idriss Moutawakil, Noëlie Sylvain, Barbara Lepage    *)
 (*									     *)
 
-(*	main of the lambda evaluator					     *)
+(*	test main of the lambda evaluator				     *)
 
 (*									     *)
-(* ## Main, example of simple AST (λx.x)2				     *)
+(* ## Main, examples of AST						     *)
 (*									     *)
 
+(* val show : res -> unit						     *)
+let show = function
+  | Lambdacalcul.Rconst n		->
+    begin
+      print_int n;
+      print_endline ""
+    end
+  | Lambdacalcul.Closure (str, e, env)	->
+    begin
+      print_string "Closure ";
+      print_endline str
+    end
+  | Lambdacalcul.Error err		-> print_endline err
+    
+(* val main : unit -> unit						     *)
 let main () =
-  (* let e = Lambdacalcul.App *)
-  (*   (Lambdacalcul.Abs ("x", Lambdacalcul.Var "x"), Lambdacalcul.Const 2) and *)
-  (* let f = Lambdacalcul.App *)
-  (*   (Lambdacalcul.App (Lambdacalcul.Abs *)
-  (* 			 ("x", Lambdacalcul.Abs ("y", Lambdacalcul.Var "x")), *)
-  (* 		       Lambdacalcul.Const 1), *)
-  (*    Lambdacalcul.Const 3) in *)
-  let e = Lambdacalcul.Abs ("x", Lambdacalcul.Var "x") in
-  let resultat = Lambdacalcul.eval_lambda [] e in
-    match resultat with
-      | Lambdacalcul.Rconst n		->
-  	  begin
-  	    print_int n;
-  	    print_endline ""
-  	  end
-      | Lambdacalcul.Closure (str, e, env)	->
-  	  begin
-  	    print_string "Closure ";
-  	    print_endline str
-  	  end
-      | Lambdacalcul.Error err		-> print_endline err
-	  
+  let env = [] in
+  begin
+
+    (* λx.x								     *)
+    show (Lambdacalcul.eval_lambda env
+	    (Lambdacalcul.Abs ("x", Lambdacalcul.Var "x")));
+
+    (* (λy.y)2								     *)
+    show (Lambdacalcul.eval_lambda env
+	    (Lambdacalcul.App(Lambdacalcul.Abs ("y", Lambdacalcul.Var "y"),
+			      Lambdacalcul.Const 2)));
+
+    (* ((λw.(λz.w))1)3							     *)
+    show (Lambdacalcul.eval_lambda env
+	    (Lambdacalcul.App
+	       ((Lambdacalcul.App
+		   (Lambdacalcul.Abs
+		      ("x",Lambdacalcul.Abs ("y",Lambdacalcul.Var "x")),
+		    Lambdacalcul.Const 1), Lambdacalcul.Const 3))));
+
+  end
+
 let _ = main ()
 
